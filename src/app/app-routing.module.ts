bcc3358a01c4c688c5bd2bb76427eec1fa09bcaf
Kibrands <Kibrands@github.com>
2020-12-28 03:36:38 +0100

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { SummonerComponent } from './summoner/summoner.component';
+
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'summoner', component: SummonerComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
