5dcf55a507a440f215181e9e699f47205f0aa080
Kibrands <Kibrands@github.com>
2020-12-30 18:03:23 +0100

    setTitle (summoner)

--- src/app/summoner/summoner.component.ts
+++ src/app/summoner/summoner.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { HttpClient } from '@angular/common/http';
 import * as jQuery from 'jquery';
+import { Title } from '@angular/platform-browser';
 
 interface Summoner {
   server: string;
@@ -18,7 +19,8 @@ export class SummonerComponent implements OnInit {
   summoner: { server: string, name: string };
   summoner$: Summoner
 
-  constructor(private route: ActivatedRoute, translate: TranslateService, private http: HttpClient) {
+  constructor(private route: ActivatedRoute, translate: TranslateService
+    , private http: HttpClient, private titleService: Title) {
     translate.setDefaultLang('en');
     translate.use(localStorage.getItem('client-lang'));
   }
@@ -34,6 +36,7 @@ export class SummonerComponent implements OnInit {
         this.summoner.name = params.name;
       }
     );
+    this.setTitle('Legends GG - ' + this.summoner.name.toUpperCase() + " (" + this.summoner.server.toUpperCase() + ")");
     this.http.get<Summoner>('https://datagg.herokuapp.com/summoner/' + this.summoner.server + "/" + this.summoner.name)
       .subscribe(data => {
         this.summoner$ = data;
@@ -43,5 +46,9 @@ export class SummonerComponent implements OnInit {
 
   printSummoner(): void {
     jQuery('#data').html('<p>' + this.summoner$.server.toUpperCase() + " - " + this.summoner$.name.toUpperCase() + '</p>');
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
   }
 }
